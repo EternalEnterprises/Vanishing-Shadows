2917abf26bed6d3854e22f77cfebc05df8385144
Aaron J. Ratcliffe <aratcli6@uncc.edu>
2016-03-16 13:26:15 -0400

    Revert "Organized files and objects"
    This reverts commit 60cd6d42d25e7dd2bfd79267dff4fd8867360a66.

---
+++ Vanishing Shadows/Assets/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public int levelCount; // need to make this global
+
+	// Use this for initialization
+	void Start () {
+		// checks to see if values have been set for levels
+		if (PlayerPrefs.GetString ("load") != null) {
+			// initializes array for checking levels as this is first time loading.
+			PlayerPrefs.SetString ("load", "foobar");
+			for (int i = 0; i < levelCount; i++) {
+				string level = i.ToString();
+				PlayerPrefs.SetInt (level, 0);
+			}
+		}
+		// checks to see if first level has been completed
+		if (PlayerPrefs.GetInt ("1") != 0) {
+			//loads the continue prefab
+			//GameObject instance = Instantiate(continue);
+			//script in the continue button will have code to check for last level completed
+		} else {
+			// instanciate Start button.
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
