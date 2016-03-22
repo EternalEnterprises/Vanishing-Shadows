5ca4a1f0f20d5bc6b9c6f56b49e9b944b81c5c2a
GWhite24 <gwhite24@uncc.edu>
2016-03-22 12:23:15 -0400

    Grumpy Update
    Fixing the menu system after several rather annoying mishaps. Should be
    fully funcitonal it just doesn't look pretty. Have to transfer work on
    checkpoint code to this git sometime later this week.

---
+++ Vanishing Shadows/Assets/Menu/Menu Scripts/ContinueCheckLoad.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ContinueCheckLoad : MonoBehaviour {
+	public int levelProgress;
+	// Use this for initialization
+	void Start () {
+		// checks to see if the first level has been completed and disables itself based on that.
+		if (PlayerPrefs.GetInt("1") != levelProgress) {
+			gameObject.SetActive(false);
+		}
+
+	}
+
+}
