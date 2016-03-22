274b5254ec40ae8191c46a2793d6b4ee80d6569a
Aaron Ratcliffe <aratcli6@uncc.edu>
2016-03-22 14:43:12 -0400

    Revert "Mechanics and ui"

---
+++ Vanishing Shadows/Assets/LoadContinue.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadContinue : MonoBehaviour {
+	public int levelCount;
+	// loads the last level to be not completed
+	public void Continue(){
+		int level = 1;
+		// loops through level list
+		for (int i = 1; i < levelCount; i++) {
+			// checks for the first insance a level has not been completed.
+			if(PlayerPrefs.GetInt(i.ToString()) == 0){
+				level = i;
+				break;
+			}
+		}
+		// loads the apropriate scene
+		Application.LoadLevel(level);
+	}
+}
--- Vanishing Shadows/Assets/Menu/Menu Scripts/LoadOnClick.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class LoadOnClick : MonoBehaviour {
-	
-	public void loadScene(int level){
-		Application.LoadLevel(level);
-	}
-}
