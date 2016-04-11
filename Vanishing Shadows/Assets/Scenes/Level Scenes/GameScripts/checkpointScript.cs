57bac98e92fff3cb2c99bce283ca9ea1a07803e0
GWhite24 <gwhite24@uncc.edu>
2016-04-11 10:56:39 -0400

    fixes to checkpoint at end of level
    altered checkpoint to load next availble level

--- Vanishing Shadows/Assets/Scenes/Level Scenes/GameScripts/checkpointScript.cs
+++ Vanishing Shadows/Assets/Scenes/Level Scenes/GameScripts/checkpointScript.cs
@@ -4,9 +4,14 @@ using UnityEngine.SceneManagement;
 
 public class checkpointScript : MonoBehaviour {
 	public int level;
+	public int levelCount = 3;
 
 	void OnTriggerEnter(Collider other){
 		PlayerPrefs.SetInt (level.ToString(), 1);
-		SceneManager.LoadScene (0);
+		if (level < levelCount) {
+			SceneManager.LoadScene (level + 1);
+		} else {
+			SceneManager.LoadScene (0);
+		}
 	}
 }
