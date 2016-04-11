e00bec10a93252bc08c6a6b97c696f273d6a7528
GWhite24 <gwhite24@uncc.edu>
2016-04-11 03:18:31 -0400

    Game mechanics Implementd
    What was done
    - levels can now be completed by reaching the end
    - camera implemented
    - pause menu implemented
    - touch guard you die
    - added meshes to most of the walls

---
+++ Vanishing Shadows/Assets/Scenes/Level Scenes/GameScripts/checkpointScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class checkpointScript : MonoBehaviour {
+	public int level;
+
+	void OnTriggerEnter(Collider other){
+		PlayerPrefs.SetInt (level.ToString(), 1);
+		SceneManager.LoadScene (0);
+	}
+}
