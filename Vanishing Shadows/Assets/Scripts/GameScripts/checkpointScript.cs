7f557f81274cc1c959ed9459db6d208394d05376
AaronRatcliffe <aratcli6@uncc.edu>
2016-04-17 20:02:29 -0400

    made it presentable for tomarow.
    I added back the third percen controler because the ninja still can not
    move corectly.

--- Vanishing Shadows/Assets/Scripts/GameScripts/checkpointScript.cs
+++ Vanishing Shadows/Assets/Scripts/GameScripts/checkpointScript.cs
@@ -6,12 +6,14 @@ public class checkpointScript : MonoBehaviour {
 	public int level;
 	public int levelCount = 3;
 
-	void OnTriggerEnter(Collider other){
-		PlayerPrefs.SetInt (level.ToString(), 1);
-		if (level < levelCount) {
-			SceneManager.LoadScene (level + 1);
-		} else {
-			SceneManager.LoadScene (0);
-		}
-	}
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            PlayerPrefs.SetInt(level.ToString(), 1);
+            if (level < levelCount) {
+                SceneManager.LoadScene(level + 1);
+            } else {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
 }
